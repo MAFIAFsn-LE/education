3e7e0d360f62973bcbfa9edfdc876350f229d388
MAFIA <azibekovacynara@gmail.com>
2026-08-08 11:50:37 +0600

    Initial commit

---
+++ src/Variables/VariableTask.java
@@ -0,0 +1,32 @@
+package Variables;
+
+public class VariableTask {
+    public static void main(String[] args) {
+//Task 1
+       /* int a = 12;
+        int b = 13;
+        System.out.println(a + "+" + b + "=" + (a + b));*/
+
+//Task 2.1
+        /*int a = 5;
+        int b = 7;
+        System.out.println(a + "*" + b + "=" + (a * b));*/
+
+//Task 2.2
+        /*int age = 17;
+        System.out.println("Ваш возраст: " + age + " лет");*/
+
+//Task 2.3
+        /*double price = 207.20;
+        int quantity = 5;
+        String name = "Orient Maestro";
+        System.out.println("Product: " + name);
+        System.out.println("Quantity: " + quantity);
+        System.out.println("Price: " + price + "$");
+        System.out.println("Total price: " + quantity * price + "$");*/
+
+
+
+
+    }
+}
---
+++ src/Variables/VariableTest.java
@@ -0,0 +1,14 @@
+package Variables;
+import java.util.Scanner;
+
+public class VariableTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+
+
+    }
+}
