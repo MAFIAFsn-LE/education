29bf398fbc9b1682fc38b1cf4d2457d75ec7449f
MAFIA <azibekovacynara@gmail.com>
2026-08-13 11:03:38 +0600

    I tested printf and ran tasks

--- src/Variables/VariableTest.java
+++ src/Variables/VariableTest.java
@@ -4,6 +4,45 @@ import java.util.Scanner;
 public class VariableTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+//Test 1
+        /*System.out.println("Diego");
+        System.out.println(3);
+        System.out.println("Fate" + "Life" + "Death ");*/
+
+//Test 2
+        /*System.out.println(1+3);
+        System.out.println("1" + "3");
+        System.out.println(1 + "3");
+        System.out.println("1" + 3);
+        System.out.println("1" + (1 + 3));*/
+
+//Test 3
+        /*System.out.printf("Hello %s !", "world");
+        System.out.printf("\nI'm %d years old!", 17);
+        System.out.printf("\nThe number Pi is equal - %.2f!", 3.14159);*/
+
+//Test 3.1
+       /* String item = "Laptop";
+        int quantity = 5;
+        System.out.printf("Bought %d psc. Product: %s.", quantity, item);
+*/
+//Test 3.2
+        /*double price = 499.998;
+        System.out.printf("Price product: %.2f", price);*/
+
+//Test 3.3
+       /* String name = "Kudaiberdi";
+        int age = 17;
+        double gpa = 4.0;
+        System.out.printf("Student: %s | Age: %d | Average score: %.1f", name, age, gpa);*/
+
+//Test 3.4
+        /*System.out.printf("Step 1: Loading... %nStep 2: Processing... %nStep 3: Ready!");*/
+
+//Test 3.5
+        /*int discount = 20;
+        System.out.printf("Yours discount is %d%%.", discount);*/
+
 
 
 
