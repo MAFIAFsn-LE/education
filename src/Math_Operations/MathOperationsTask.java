85b045330336e3e0aa78db73afd7b68e09188b79
MAFIA <azibekovacynara@gmail.com>
2026-08-16 23:12:13 +0600

    I did tasks and study

---
+++ src/Math_Operations/MathOperationsTask.java
@@ -0,0 +1,49 @@
+package Math_Operations;
+import java.util.Scanner;
+import java.util.Random;
+
+public class MathOperationsTask {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+//Task 1
+        /*System.out.println("Enter your Name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter you Surname: ");
+        String surname = scanner.nextLine();
+
+        int todayYears = 2026;
+        System.out.println("Enter your year of birth: ");
+        int yearOfBirth = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Enter the year you started working: ");
+        int yearStartedWorking = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.printf("Name: %s%nSurname: %s%nAge: %d%nWork experience: %d",
+                name, surname, todayYears - yearOfBirth, todayYears - yearStartedWorking );*/
+
+//Task2
+        /*double num = 3.5;
+        System.out.printf("Integer: %.1f%nRemainder: %.1f%n", num, (num - (int)num));
+        double num2 = 4.7;
+        System.out.printf("Integer: %.1f%nRemainder: %.1f%n", num2, (num2 - (int)num2));
+        double num3 = 2.4;
+        System.out.printf("Integer: %.1f%nRemainder: %.1f%n", num3, (num3 - (int)num3));*/
+
+//Task 3
+       /*     int num = random.nextInt(10) + 1;
+            int num2 = random.nextInt(10) + 1;
+            int num3 = random.nextInt(10) + 1;
+
+            double average = (num + num2 + num3) / 3.0;
+
+            System.out.printf("1)Random num: %d%n2)Random num: %d%n3)Random num: %d%nArithmetic mean: %.1f ",
+                    num, num2, num3, average);*/
+
+
+
+
+    }
+}
---
+++ src/Math_Operations/MathOperationsTest.java
@@ -0,0 +1,26 @@
+package Math_Operations;
+
+public class MathOperationsTest {
+    public static void main(String[] args) {
+//Test 1
+       /* int num = -8;
+        int positiveNum = Math.abs(num);
+        positiveNum++;
+        System.out.printf("Before: %d%nAfter: %d", num, positiveNum);*/
+
+//Test 2
+       /* double base = 3.0;
+        double squared = Math.pow(base, 2);
+        double root = Math.sqrt(squared);
+        root--;
+        System.out.println(root);*/
+
+//Test 3
+        /*int x = 15;
+        int y = 42;
+        int max = Math.max(x, y);
+        int min = Math.min(x, y);
+        min++;
+        System.out.printf("Number max: %d%nNumber min: %d", max, min);*/
+    }
+}
